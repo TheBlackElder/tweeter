8503c679a5cf5e0d8391d197dc32b1db27639417
TheBlackElder <freedxcollective@gmail.com>
2023-01-31 20:22:53 -0500

    text boxt and new tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   console.log("We are ready!");
 
   $("#tweet-text").on("input", function () {
-    let count = $('.tweet-counter');
+    let count = $(".tweet-counter");
     let maxChars = 140;
     let charsEntered = $("#tweet-text").val().length;
     let charsRemaining = maxChars - charsEntered;
-    $(".tweet-counter").text(charsRemaining);
+    count.text(charsRemaining);
     if (charsEntered > maxChars) {
       count.addClass("countLimit");
     } else {
@@ -14,3 +14,23 @@ $(document).ready(function () {
     }
   });
 });
+
+// $(document).ready(function () {
+//   console.log("We are ready!");
+
+//   $("#tweet-text").on("input", function () {
+//     let maxChars = 10;
+//     const input = $(this);
+//     const form = input.closest('.counter');
+//     const counter = form.find('.counter');
+//     let charsEntered = input.val().length;
+//     let charsRemaining = maxChars - charsEntered;
+//     counter.text(charsRemaining);
+
+//     if (charsRemaining < 0) {
+//       counter.addClass("countLimit");
+//     } else {
+//       counter.removeClass("countLimit");
+//     }
+//   });
+// });
